d062f6f54acf012821000e5ea552bcbd0c65824c
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-12-04 17:35:29 -0300

    Actualizando

--- Clase 14/scripts.js
+++
@@ -1,8 +0,0 @@
-/* Para pedir un dato al usuario se puede usar prompt(mensaje) */
-
-prompt('Introduzca su nombre')
-
-/* Para imprimir un dato se puede usar console.log(dato) */
-
-console.log(dato);
-
--- Clase-09-arrays-introduccion/scripts.js
+++ Clase-09-arrays-introduccion/scripts.js
@@ -1,7 +1,6 @@
 /* 
  - Son estructuras que nos permiten almacenar varios datos y agruparlos.
  - Se pueden llenar con cualquier tipo de dato válido en JavaScript y deben ir separados por comas
- - Se pueden mezclar tipos de datos, pero no es recomendable.
  - Se declaran con llaves cuadradas o corchetes []
  - Pueden declararse vacíos o con un contenido ya establecido
  - Pueden añadirse o eliminarse elementos en el momento que queramos
